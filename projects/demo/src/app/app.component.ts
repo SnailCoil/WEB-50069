d028502dac8d3e953896214145ee55bd78585693
Aaron Snailwood <asherwood@collegenet.com>
2022-09-30 08:43:54 -0700

    add comment about template property of app component

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // If you swap template out for templateUrl, then you can type "<lib1-" and it will autocomplete correctly.
+  // template: ''
   templateUrl: 'app.component.html',
 })
 export class AppComponent {
